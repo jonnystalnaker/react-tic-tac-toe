bf742f4ea931be9b7e0dc1d520d57e6c2eaecc81
jonnystalnaker <steamsynthetic@gmail.com>
2024-05-02 15:13:12 -0700

    made header static. player cards into component

---
+++ src/components/Player.jsx
@@ -0,0 +1,11 @@
+﻿export default function Player({ name, synbol }) {
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">{name}</span>
+        <span className="player-symbol">{synbol}</span>
+      </span>
+      <button>Edit</button>
+    </li>
+  );
+}
